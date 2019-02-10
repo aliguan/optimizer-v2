9dcc3d410fffe8412705215d43128044d2f44f87
Alice Guan <aliguan726@gmail.com>
2019-02-10 17:18:36 -0500

    more footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,47 +13,50 @@ export class Footer extends Component {
 
 
     render() {
-
         return (
-            <div className="row eventsCont footer">
-                <div className="col-md-2 offset-md-2">
-                    <ul className="footer-1">
-                        <li><strong>BLUE</strong> PLANIT</li>
-                        <li>About Us</li>
-                        <li><a href="http://blue-planit.com/" target="_blank">Blog</a></li>
-                        <li>Contact</li>
-                    </ul>
-
+            <div className="footer">
+                <div class="footer-content">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <ul className="footer-1">
+                                <li><strong>BLUE</strong> PLANIT</li>
+                                <li>About Us</li>
+                                <li><a href="http://blue-planit.com/" target="_blank">Blog</a></li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3">
+                            <ul>
+                                <li><strong>LEGAL</strong></li>
+                                <li>Privacy</li>
+                                <li>Terms</li>
+                                <li>Site Map</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3">
+                            <ul className="footer-2">
+                                <li><strong>POWERED BY</strong></li>
+                                <li><a href="http://meetup.com">Meetup</a></li>
+                                <li> <a href="http://eventbrite.com">Eventbrite</a></li>
+                                <li><a href="http://google.com">Google Places</a></li>
+                                <li><a href="http://seatgeek.com">Seatgeek</a></li>
+                                <li><a href="http://yelp.com">Yelp</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3">
+                            <ul className="social-icons">
+                                <li><strong>CONNECT WITH US</strong></li>
+                                <div>
+                                    <li className="hover-item"><i className="fab fa-facebook-square"></i></li>
+                                    <li className="hover-item"><i className="fab fa-instagram"></i></li>
+                                    <li className="hover-item"><i className="fab fa-youtube"></i></li>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
 
                 </div>
-              <div className="col-md-2">
-                  <ul>
-                      <li><strong>LEGAL</strong></li>
-                      <li>Privacy</li>
-                      <li>Terms</li>
-                      <li>Site Map</li>
-                  </ul>
-
-              </div>
-            <div className="col-md-2">
-                <ul className="social-icons">
-                    <li><strong>CONNECT WITH US</strong></li>
-                    <li className="hover-item"><i className="fab fa-facebook-square"></i></li>
-                    <li className="hover-item"><i className="fab fa-instagram"></i></li>
-                    <li className="hover-item"><i className="fab fa-youtube"></i></li>
-                </ul>
             </div>
-              <div className="col-md-2">
-                  <ul className="footer-2">
-                      <li><strong>POWERED BY</strong></li>
-                      <li className="hover-item"><a href="http://meetup.com">Meetup</a></li>
-                      <li className="hover-item"> <a href="http://eventbrite.com">Eventbrite</a></li>
-                      <li className="hover-item"><a href="http://google.com">Google Places</a></li>
-                      <li className="hover-item"><a href="http://seatgeek.com">Seatgeek</a></li>
-                      <li className="hover-item"><a href="http://yelp.com">Yelp</a></li>
-                  </ul>
-              </div>
-          </div>
         )
     }
 }
